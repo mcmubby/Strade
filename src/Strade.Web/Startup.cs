6e86f43b5a92fb5a4464aa5cc42c48099f4b4036
Mubarak Ayelotan <12859241+mcmubby@users.noreply.github.com>
2020-08-16 17:23:47 +0100

    database setup

--- src/Strade.Web/Startup.cs
+++ src/Strade.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Strade.Data.DbContexts;
 
 namespace Strade.Web
 {
@@ -23,6 +25,14 @@ namespace Strade.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("Auth"), 
+            sqlServerOption => sqlServerOption.MigrationsAssembly("Strade.Data")));
+
+            services.AddDbContextPool<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("App"), 
+            sqlServerOption => sqlServerOption.MigrationsAssembly("Strade.Data")));
+
             services.AddControllersWithViews();
         }
 
