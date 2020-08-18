4848c62051153b06577531946c923b2d4200aff4
Mubarak Ayelotan <12859241+mcmubby@users.noreply.github.com>
2020-08-18 18:08:36 +0100

    login

--- src/Strade.Web/Controllers/HomeController.cs
+++ src/Strade.Web/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Strade.Web.Models;
 
@@ -18,20 +13,18 @@ namespace Strade.Web.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Index(LoginViewModel model)
         {
+            if(!ModelState.IsValid) return View();
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
---
+++ src/Strade.Web/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace Strade.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string MatricNo { get; set; }
+        public string Password { get; set; }
+    }
+}
